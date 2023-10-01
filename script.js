296fbaf1b7d069d0d624970f6a94f3b8a5067b33
ejovwogfreeman <ejovwogfreeman007@gmail.com>
2023-10-01 22:45:17 +0100

    message

--- script.js
+++ script.js
@@ -23,19 +23,50 @@ element1.addEventListener("mouseout", () => {
 });
 
 function moveAssistant(targetElement, buttonNumber) {
+  clearAssistant(); // Clear previous content
   const targetRect = targetElement.getBoundingClientRect();
   assistant.style.left = `${targetRect.left}px`;
   assistant.style.top = `${targetRect.top}px`;
-  assistant.textContent = `Button ${buttonNumber} clicked`;
+
+  // Create a new container for the image and text
+  const imgContainer = document.createElement("div");
+  const img = document.createElement("img");
+  img.src = "walking.gif";
+  img.alt = "Walking Icon";
+
+  // Append the image and text to the new container
+  imgContainer.appendChild(img);
+  imgContainer.appendChild(
+    document.createTextNode(`Button ${buttonNumber} clicked`)
+  );
+
+  // Append the new container to the assistant
+  assistant.appendChild(imgContainer);
   assistant.style.opacity = "1";
 }
 
 function showExplanation(element, text) {
-  assistant.textContent = text;
+  clearAssistant(); // Clear previous content
+  const imgContainer = document.createElement("div");
+  const img = document.createElement("img");
+  img.src = "walking.gif";
+  img.alt = "Walking Icon";
+
+  imgContainer.appendChild(img);
+  imgContainer.appendChild(document.createTextNode(text));
+
+  assistant.appendChild(imgContainer);
   assistant.style.opacity = "1";
 }
 
 function hideExplanation() {
-  assistant.textContent = "";
+  clearAssistant(); // Clear previous content
   assistant.style.opacity = "0";
+}
+
+function clearAssistant() {
+  // Remove all child elements from the assistant
+  while (assistant.firstChild) {
+    assistant.removeChild(assistant.firstChild);
+  }
 }
