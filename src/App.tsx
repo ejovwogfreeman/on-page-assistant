eab18e11b40d02119a54140844c8509dc2fa7299
ejovwogfreeman <ejovwogfreeman007@gmail.com>
2023-10-02 16:36:29 +0100

    message

---
+++ src/App.tsx
@@ -0,0 +1,200 @@
+import { useRef, useState } from "react";
+import "./App.scss";
+import profile from "./hair";
+import techie from "./assets/bubble.gif";
+import { useLottie } from "lottie-react";
+import animationData from "./assets/animation_ln7fp7v2.json";
+
+function App() {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+  const { View } = useLottie(defaultOptions);
+  const [restarted, setIsResetScheduled] = useState(false);
+  const [popup, setPopup] = useState(false);
+  const [popupMsg, setPopupMsg] = useState("Hello");
+  const resetTimeoutRef = useRef<ReturnType<typeof setTimeout> | null>(null);
+  //
+  const [ballPosition, setBallPosition] = useState<{
+    x: number;
+    y: number;
+  }>({ x: 0, y: 0 });
+
+  // changes and reset ball position
+  const initialX = window.innerWidth - 200;
+  const initialY = window.innerHeight - 200;
+
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const messageData = (e.target as HTMLElement).getAttribute("data-message");
+    setPopupMsg(messageData || "");
+
+    const targetX = e.pageX;
+    const targetY = e.pageY;
+    const clampedX = Math.max(0, Math.min(initialX, targetX));
+
+    setBallPosition({ x: clampedX, y: targetY });
+    setIsResetScheduled(true);
+    setPopup(true);
+
+    // im clearing the previoustimeout if exists
+    if (resetTimeoutRef.current) {
+      clearTimeout(resetTimeoutRef.current);
+    }
+
+    // reset after 10 seconds (10,000 milliseconds)
+    const resetTimeout = setTimeout(() => {
+      setBallPosition({ x: 0, y: 0 });
+      setIsResetScheduled(false);
+      setPopup(false);
+    }, 10000);
+    resetTimeoutRef.current = resetTimeout;
+  };
+
+  const Ball = (
+    <div
+      className="ball"
+      style={{
+        transform: `translate(${
+          ballPosition.x ? ballPosition.x : initialX
+        }px, ${ballPosition.y ? ballPosition.y : initialY}px)`,
+        position: (restarted && "absolute") || "fixed",
+      }}
+    >
+      <div className="clip">{View}</div>
+
+      {popup && (
+        <div className="assistantMsg">
+          <p>{popupMsg}</p>
+        </div>
+      )}
+    </div>
+  );
+
+  const IconBTN = ({ data = "Explain Button" }: { data?: string }) => (
+    <span data-message={data} className="icon" onClick={handleClick}>
+      <Icon data={data} />
+    </span>
+  );
+  return (
+    <>
+      {Ball}
+
+      <div className="App">
+        <nav className="nav">
+          <div className="container navWrapper">
+            <ul className="navList">
+              <li>Home</li>
+              <li>About</li>
+              <li>Policy</li>
+              <li>Contact</li>
+            </ul>
+
+            <div className="flexBTN">
+              <button className="btn" data-assist="This is an explain button">
+                Explain
+              </button>
+              <IconBTN />
+            </div>
+          </div>
+        </nav>
+        <section className="container heroSection">
+          <div className="">
+            <div className="content">
+              <h1 className="heroTitle">
+                Learn a Tech Skill and become a pro within months...
+              </h1>
+              <p>
+                Do you feel lost and uncertain about where to begin? Worry not,
+                for our dedicated online assistant is readily available to guide
+                you on a comprehensive tour of our website. You'll navigate with
+                confidence and ease under its expert guidance
+              </p>
+              <div className="heroBtn flexBTN">
+                <button className="btn" data-assist="hh">
+                  Explore
+                </button>
+                <IconBTN data="Welcome! I'm your friendly online assistant here to make your experience enjoyable and hassle-free. Feel free to ask questions or seek guidance anytime. Let's embark on this journey together!" />
+              </div>
+            </div>
+          </div>
+          <div className="">
+            <img draggable="false" src={techie} style={{ width: "100%" }} />
+          </div>
+        </section>
+        <section className="cardsSection">
+          <div className="container hairCards">
+            {profile.map(
+              (
+                item: {
+                  image: string;
+                  name: string;
+                  data?: string;
+                },
+                index
+              ) => (
+                <div className="hairCard" key={index}>
+                  <img
+                    src={item.image}
+                    style={{
+                      width: "100%",
+                      height: "300px",
+                      objectFit: "cover",
+                    }}
+                  />
+                  <div className="info">
+                    <h3 style={{ fontWeight: "bold" }}>{item.name}</h3>
+                    <IconBTN data={item.data} />
+                  </div>
+                </div>
+              )
+            )}
+          </div>
+        </section>
+      </div>
+
+      <footer>
+        <p>On Page Assistant HNGx</p>
+      </footer>
+    </>
+  );
+}
+
+function Icon({ data }: { data: string }) {
+  return (
+    <svg
+      data-message={data}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <mask
+        data-message={data}
+        id="mask0_302_86"
+        style={{ maskType: "alpha" }}
+        width="24"
+        height="24"
+        x="0"
+        y="0"
+        maskUnits="userSpaceOnUse"
+      >
+        <path fill="#D9D9D9" d="M0 0H24V24H0z"></path>
+      </mask>
+      <g data-message={data} mask="url(#mask0_302_86)">
+        <path
+          data-message={data}
+          fill="#fff"
+          d="M11.95 18c.35 0 .646-.12.888-.363.241-.241.362-.537.362-.887s-.12-.646-.362-.887a1.207 1.207 0 00-.888-.363c-.35 0-.646.12-.887.363a1.207 1.207 0 00-.363.887c0 .35.12.646.363.887.241.242.537.363.887.363zm-.9-3.85h1.85c0-.55.063-.983.188-1.3.125-.317.479-.75 1.062-1.3a7.494 7.494 0 001.025-1.238c.25-.391.375-.862.375-1.412 0-.933-.342-1.65-1.025-2.15-.683-.5-1.492-.75-2.425-.75-.95 0-1.72.25-2.313.75-.591.5-1.004 1.1-1.237 1.8l1.65.65c.083-.3.27-.625.563-.975.291-.35.737-.525 1.337-.525.533 0 .933.146 1.2.437.267.292.4.613.4.963 0 .333-.1.646-.3.938-.2.291-.45.562-.75.812-.733.65-1.183 1.142-1.35 1.475-.167.333-.25.942-.25 1.825zM12 22a9.738 9.738 0 01-3.9-.788 10.099 10.099 0 01-3.175-2.137c-.9-.9-1.612-1.958-2.137-3.175A9.738 9.738 0 012 12c0-1.383.263-2.683.788-3.9a10.099 10.099 0 012.137-3.175c.9-.9 1.958-1.612 3.175-2.137A9.738 9.738 0 0112 2c1.383 0 2.683.263 3.9.788a10.098 10.098 0 013.175 2.137c.9.9 1.613 1.958 2.137 3.175A9.738 9.738 0 0122 12a9.738 9.738 0 01-.788 3.9 10.098 10.098 0 01-2.137 3.175c-.9.9-1.958 1.613-3.175 2.137A9.738 9.738 0 0112 22zm0-2c2.233 0 4.125-.775 5.675-2.325C19.225 16.125 20 14.233 20 12c0-2.233-.775-4.125-2.325-5.675C16.125 4.775 14.233 4 12 4c-2.233 0-4.125.775-5.675 2.325C4.775 7.875 4 9.767 4 12c0 2.233.775 4.125 2.325 5.675C7.875 19.225 9.767 20 12 20z"
+        ></path>
+      </g>
+    </svg>
+  );
+}
+
+export default App;
---
+++ src/hair.ts
@@ -0,0 +1,59 @@
+import office from "./assets/microsoft.jpg";
+import graphic from "./assets/graphic.jpg";
+import uiux from "./assets/uiux.jpg";
+import webdev from "./assets/web.jpg";
+import datasci from "./assets/datasci.jpg";
+import dataana from "./assets/dataana.jpg";
+import mobile from "./assets/mobile.png";
+import hacking from "./assets/hacking.jpg";
+import comptia from "./assets/comptia.jpg";
+
+const hair = [
+  {
+    name: "MS OFFICE",
+    image: office,
+    data: "Learning Microsoft Office is essential for efficient work in any office setting. It includes tools like Word for document creation, Excel for data analysis, and PowerPoint for presentations. Proficiency in MS Office boosts productivity and employability, making it a valuable skill for professionals in various industries.",
+  },
+  {
+    name: "GRAPHICS",
+    image: graphic,
+    data: "Learning graphics involves mastering visual communication through digital tools. It encompasses skills like image editing, vector graphics, and layout design. Proficiency in graphic design software like Adobe Photoshop and Illustrator is crucial for creating stunning visuals, from marketing materials to digital art, enhancing creativity and communication across various industries.",
+  },
+  {
+    name: "UI/UX",
+    image: uiux,
+    data: "Learning UI/UX (User Interface/User Experience) design is essential for creating user-friendly digital experiences. It involves designing intuitive interfaces, optimizing user journeys, and enhancing user satisfaction. Understanding UI/UX principles, wireframing, and prototyping is crucial for designing websites and apps that resonate with users and meet their needs effectively.",
+  },
+  {
+    name: "WEB DEVELOPMENT",
+    image: webdev,
+    data: "Learning web development is mastering the art of creating websites and web applications. It involves coding languages like HTML, CSS, and JavaScript, enabling you to build interactive and responsive sites. Proficiency in frameworks and libraries, such as React or Angular, enhances your ability to create dynamic web solutions, making you a valuable developer in the digital world.",
+  },
+  {
+    name: "DATA SCIENCE",
+    image: datasci,
+    data: "Learning data science involves acquiring skills in data analysis, machine learning, and statistics. It empowers you to extract valuable insights from large datasets, enabling data-driven decision-making. Proficiency in tools like Python and data visualization libraries like Matplotlib equips you to solve complex problems and contribute to various industries, including healthcare, finance, and technology.",
+  },
+  {
+    name: "DATA ANALYTICS",
+    image: dataana,
+    data: "Learning data analytics involves mastering techniques to examine and interpret data, helping organizations make informed decisions. It includes data cleaning, statistical analysis, and data visualization using tools like Excel, SQL, and Tableau. Data analysts play a crucial role in transforming raw data into actionable insights, supporting business growth and strategy.",
+  },
+  {
+    name: "MOBILE DEVELOPMENT",
+    image: mobile,
+    data: "Learning mobile development equips you with the skills to create applications for smartphones and tablets. It involves mastering programming languages like Java or Swift, mobile app frameworks, and UI/UX design. Mobile developers build apps for iOS and Android platforms, catering to a vast and growing user base, and driving innovation in the digital world.",
+  },
+  {
+    name: "ETHICAL HACKING",
+    image: hacking,
+    data: "Learning ethical hacking is the pursuit of cybersecurity expertise to safeguard digital systems and networks. Ethical hackers, also known as penetration testers or white-hat hackers, use their skills to identify vulnerabilities and strengthen security. They play a critical role in protecting organizations and individuals from malicious cyber threats by actively testing and improving defenses.",
+  },
+  {
+    name: "COMPTIA A+",
+    image: comptia,
+    data: "CompTIA A+ is a foundational IT certification that validates essential skills in IT support and technical troubleshooting. Learning CompTIA A+ equips individuals with knowledge in hardware, software, networking, and security, making them proficient in maintaining and troubleshooting computer systems. It's a valuable credential for entry-level IT professionals and a stepping stone to advanced certifications.",
+  },
+];
+
+export default hair;
